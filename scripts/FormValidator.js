58385497fce6eb56150c2da77ea33ad7f2607946
COOLeR7up <cool.chem84@mail.ru>
2020-09-14 22:38:16 +0800

    class second try

---
+++ scripts/FormValidator.js
@@ -0,0 +1,18 @@
+class FormValidator {
+    constructor(settings) {
+        this.form = settings.form
+        this.input = settings.input
+        this.errorSelector = settings.errorSelector
+        this.controlSelector = settings.controlSelector
+        this.errorMessage = settings.errorMessage
+    }
+
+
+
+}
+
+
+const settings = {
+
+}
+const formValidator = new FormValidator(settings)
